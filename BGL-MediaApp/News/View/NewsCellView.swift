ef206dab7a41d38b6fb540925e694e17ad4edd03
TonyZXY <xyzheng109@gmail.com>
2018-06-04 17:55:07 +1000

    News Refector

---
+++ BGL-MediaApp/News/View/NewsCellView.swift
@@ -0,0 +1,21 @@
+//
+//  NewsCellView.swift
+//  BGL-MediaApp
+//
+//  Created by Xuyang Zheng on 4/6/18.
+//  Copyright © 2018 Xuyang Zheng. All rights reserved.
+//
+
+import UIKit
+
+class NewsCellView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
