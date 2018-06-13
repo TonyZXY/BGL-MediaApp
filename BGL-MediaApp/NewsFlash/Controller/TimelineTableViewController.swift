5eca0d3d0ddcc276aa3a78717c244694c8553f8a
“brucegfz@gmail.com” <brucegfz@gmail.com>
2018-06-13 18:02:17 +1000

    Merge FlashNews

---
+++ BGL-MediaApp/NewsFlash/Controller/TimelineTableViewController.swift
@@ -0,0 +1,290 @@
+//
+//  TimelineTableViewController.swift
+//  news app for blockchain
+//
+//  Created by Sheng Li on 23/4/18.
+//  Copyright © 2018 Sheng Li. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+import Alamofire
+import RealmSwift
+
+class TimelineTableViewController: UITableViewController {
+    
+    let realm = try! Realm()
+    var results = try! Realm().objects(NewsFlash.self).sorted(byKeyPath: "dateTime", ascending: false)
+    
+    var inSearchMode = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let timelineTableViewCellNib = UINib(nibName: "TimelineTableViewCell", bundle: Bundle(for: TimelineTableViewCell.self))
+        self.tableView.register(timelineTableViewCellNib, forCellReuseIdentifier: "TimelineTableViewCell")
+        
+        //Prevent empty rows
+        self.tableView.tableFooterView = UIView()
+        self.tableView.backgroundColor = ThemeColor().themeColor()
+        self.tableView.separatorStyle = .none
+        self.tableView.addSubview(self.refresher)
+        
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        getNews()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if inSearchMode {
+            return 4
+        }
+        return results.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TimelineTableViewCell", for: indexPath) as! TimelineTableViewCell
+        let object = results[indexPath.row]
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM d, yyyy, h:ma"
+        
+        let bglGreen = #colorLiteral(red: 0.5019607843, green: 0.8588235294, blue: 0.7176470588, alpha: 1)
+        
+        cell.timelinePoint = TimelinePoint(diameter: CGFloat(16.0), color: bglGreen, filled: false)
+        cell.timelinePointInside = TimelinePoint(diameter: CGFloat(4.0), color: bglGreen, filled: true, insidePoint: true)
+        cell.timeline.backColor = #colorLiteral(red: 0.7294117647, green: 0.7294117647, blue: 0.7294117647, alpha: 1)
+        cell.titleLabel.text = dateFormatter.string(from: object.dateTime)
+        cell.descriptionLabel.text = object.contents
+//        cell.likeButton.tag = indexPath.row
+//        cell.likeButton.addTarget(self, action: #selector(likeButtonClicked), for: .touchUpInside)
+        cell.object = object
+        cell.shareButton.addTarget(self, action: #selector(shareButtonClicked), for: .touchUpInside)
+        
+        return cell
+    }
+    
+    @objc func likeButtonClicked(sender: UIButton){
+//        print(sender.tag)
+        
+        let index = IndexPath(row: sender.tag, section: 0)
+        let cell:TimelineTableViewCell = self.tableView.cellForRow(at: index) as! TimelineTableViewCell
+        
+//        print(cell.likeButton.currentTitle)
+        if cell.likeButton.currentTitle == "♡" {
+            cell.likeButton.setTitle("❤️", for: UIControlState.normal)
+        }else{
+            cell.likeButton.setTitle("♡",for: UIControlState.normal)
+        }
+        
+    }
+    
+    @objc func shareButtonClicked(sender: UIButton){
+        
+        let buttonPosition:CGPoint = sender.convert(CGPoint(x: 0, y: 0), to:self.tableView)
+        let indexPath = self.tableView.indexPathForRow(at: buttonPosition)
+        let cell = tableView.cellForRow(at: indexPath!)! as! TimelineTableViewCell
+        let cellText = cell.descriptionLabel.text
+        let size = cell.descriptionLabel.font.pointSize
+        let textImage = self.textToImage(drawText: cellText!, inImage: cell.descriptionLabel.createImage!, atPoint: CGPoint(x:0, y:0), withSize:size)
+    
+        let topImage = combineLogoWithText(combine: UIImage(named: "bcg_logo.png")!, with: textImage)
+        let bottomImage = UIImage(named: "sample_qr_code.png")
+        let image = combineImageWithQRCode(combine: topImage, with: (bottomImage)!)
+        
+        let activityVC = UIActivityViewController(activityItems: [image], applicationActivities:nil)
+        activityVC.popoverPresentationController?.sourceView = self.view
+        self.present(activityVC,animated: true, completion:nil)
+    }
+    
+    func textToImage(drawText text: String, inImage image: UIImage, atPoint point: CGPoint, withSize size: CGFloat) -> UIImage {
+        let textColor = UIColor.black
+        
+        let textFont = UIFont.systemFont(ofSize: size)
+        
+        let scale = UIScreen.main.scale
+        UIGraphicsBeginImageContextWithOptions(image.size, false, scale)
+        
+        let textFontAttributes = [
+            NSAttributedStringKey.font: textFont,
+            NSAttributedStringKey.foregroundColor: textColor,
+            ] as [NSAttributedStringKey : Any]
+        image.draw(in: CGRect(origin: CGPoint.zero, size: image.size))
+        
+        let rect = CGRect(origin: point, size: image.size)
+        text.draw(in: rect, withAttributes: textFontAttributes)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+    func combineImageWithQRCode(combine topImage:UIImage, with bottomImage:UIImage)-> UIImage{
+        let width = topImage.size.width
+        let height = topImage.size.height * 2
+        let size = CGSize(width: width, height: height)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        
+        topImage.draw(in: CGRect(x:0, y:0, width:width, height: height/2 ))
+        bottomImage.draw(in: CGRect(x:(width-height/2)/2, y:height/2, width: height/2,  height:height/2 ))
+        
+        let newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        // Change UIImageView requirements here
+        let mergeImageView = UIImageView(frame: CGRect(x:0, y: 200, width: 30, height: 20))
+        
+        //Combine images into a single image view.
+        mergeImageView.image = newImage
+        return mergeImageView.image!
+    }
+    
+    func combineLogoWithText(combine topImage:UIImage, with bottomImage:UIImage)-> UIImage{
+        let width = bottomImage.size.width
+        let height = bottomImage.size.height * 2
+        let size = CGSize(width: width, height: height)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        
+        topImage.draw(in: CGRect(x:(width-height/2)/2, y:0, width:height/2, height: height/2 ))
+        bottomImage.draw(in: CGRect(x:0, y:height/2, width: width,  height:height/2 ))
+        
+        let newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        // Change UIImageView requirements here
+        let mergeImageView = UIImageView(frame: CGRect(x:0, y: 200, width: 30, height: 20))
+        
+        //Combine images into a single image view.
+        mergeImageView.image = newImage
+        return mergeImageView.image!
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.backgroundColor = UIColor.clear
+        cell.selectionStyle = .none
+    }
+  
+    
+    private func getNews() {
+        Alamofire.request("http://10.10.6.111:3000/api/flash?languageTag=EN&CN", method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                self.JSONtoData(json: json)
+                DispatchQueue.main.async {
+                    self.cleanOldNewsFlash()
+                    self.results = try! Realm().objects(NewsFlash.self).sorted(byKeyPath: "dateTime", ascending: false)
+                    self.tableView.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    private func JSONtoData(json: JSON) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        realm.beginWrite()
+        if let collection = json.array {
+            for item in collection {
+                let date = dateFormatter.date(from: item["publishedTime"].string!)
+                let id = "\(item["_id"].string!)"
+                if realm.object(ofType: NewsFlash.self, forPrimaryKey: id) == nil {
+                    realm.create(NewsFlash.self, value: [id, date!, item["shortMassage"].string!])
+                } else {
+                    realm.create(NewsFlash.self, value: [id, date!, item["shortMassage"].string!], update: true)
+                }
+            }
+        }
+        try! realm.commitWrite()
+    }
+    
+    lazy var refresher: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(self.handleRefresh(_:)), for: .valueChanged)
+        refreshControl.tintColor = UIColor.gray
+        
+        return refreshControl
+    }()
+    
+    func cleanOldNewsFlash() {
+        //        let oneWeekBefore = Date.init(timeIntervalSinceNow: -(86400*7))
+        //        let oldObjects = realm.objects(NewsFlash.self).filter("dateTime < %@", oneWeekBefore)
+        //
+        //        try! realm.write {
+        //            realm.delete(oldObjects)
+        //        }
+    }
+    
+    
+    @objc func handleRefresh(_ refreshControl: UIRefreshControl) {
+        getNews()
+        self.refresher.endRefreshing()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text == nil || searchBar.text == "" {
+            
+            inSearchMode = false
+            
+            view.endEditing(true)
+            
+            tableView.reloadData()
+            
+        } else {
+            
+            inSearchMode = true
+            
+            tableView.reloadData()
+        }
+    }
+}
+
+extension UIView {
+    var createImage : UIImage? {
+        
+        let rect = CGRect(x:0, y:0, width:bounds.size.width, height:bounds.size.height)
+        
+        UIGraphicsBeginImageContextWithOptions(bounds.size, false, 0)
+        UIColor.white.set()
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+        
+    }
+}
+
+extension UIImage {
+    func resizeImage(_ newSize: CGSize) -> UIImage? {
+        func isSameSize(_ newSize: CGSize) -> Bool {
+            return size == newSize
+        }
+        
+        func scaleImage(_ newSize: CGSize) -> UIImage? {
+            func getScaledRect(_ newSize: CGSize) -> CGRect {
+                let ratio   = max(newSize.width / size.width, newSize.height / size.height)
+                let width   = size.width * ratio
+                let height  = size.height * ratio
+                return CGRect(x: 0, y: 0, width: width, height: height)
+            }
+            
+            func _scaleImage(_ scaledRect: CGRect) -> UIImage? {
+                UIGraphicsBeginImageContextWithOptions(scaledRect.size, false, 0.0);
+                draw(in: scaledRect)
+                let image = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
+                UIGraphicsEndImageContext()
+                return image
+            }
+            return _scaleImage(getScaledRect(newSize))
+        }
+        
+        return isSameSize(newSize) ? self : scaleImage(newSize)!
+    }
+}
